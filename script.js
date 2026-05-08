ace3d3c19307af342d623cd8e2419ca3ed673664
Emanuele Favero <emafaux89@gmail.com>
2026-05-08 23:43:43 +0200

    feat: show output also in the page, not only in console

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+// Constants
 const PRICE_PER_KM = 0.21;
 const MINOR_DISCOUNT = 0.2;
 const SENIOR_DISCOUNT = 0.4;
 const MINOR_AGE = 18;
 const SENIOR_AGE = 65;
 
+// DOM elements
+const output = document.getElementById('output');
+
 console.log('Calcolo del prezzo biglietto treno');
 
-// Get user input
+// User input
 const km = Number(prompt('Quanti km vuoi percorrere?'));
 const age = Number(prompt('Quanti anni hai?'));
 
-// Validate input
+// Validation
 const isValidInput = !isNaN(km) && !isNaN(age) && km > 0 && age > 0;
 
 if (isValidInput) {
@@ -19,14 +23,21 @@ if (isValidInput) {
   if (age < MINOR_AGE) discount = MINOR_DISCOUNT;
   else if (age >= SENIOR_AGE) discount = SENIOR_DISCOUNT;
 
-  // Calculate price and format it to 2 decimal places
+  // Price calculation
   const basePrice = km * PRICE_PER_KM;
   const price = basePrice * (1 - discount);
   const formattedPrice = price.toFixed(2);
 
-  console.log(`Il prezzo del biglietto è €${formattedPrice}`);
+  // Success Output
+  const resultMessage = `Il prezzo del biglietto è €${formattedPrice}`;
+  console.log(resultMessage);
+  output.textContent = resultMessage;
 } else {
-  console.log("Per favore, inserisci un numero valido per i km e l'età.");
+  // Error Output
+  const errorMessage =
+    "Per favore, inserisci un numero valido per i km e l'età.";
+  console.log(errorMessage);
+  output.textContent = errorMessage;
 }
 
 // -------------------
